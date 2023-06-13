bfe10afc29f53c5177f6dc99294dbbdb1184f55b
AriyaLyn <136447753+AriyaLyn@users.noreply.github.com>
2023-06-13 13:27:01 +0000

    Updated title and color

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -6,8 +6,8 @@
         <div id="right"></div>
         <div id="top"></div>
         <div id="bottom"></div>
-        <b-navbar-item id="fontnavbar" class="title is-2">
-          Milligram
+        <b-navbar-item id="fontnavbar" class="title is-2" style="color: rgb(25, 141, 177);">
+          Ariagram
         </b-navbar-item>
       </template>
     </b-navbar>
